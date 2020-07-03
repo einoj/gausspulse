7f6b1cf187cf84342909a62476265d35c1be05c1
einoj <einoj@users.noreply.github.com>
2020-07-03 15:29:26 +0200

    Replace magic numbers with LEN/2

--- example/gaussian_pulse_example.c
+++ example/gaussian_pulse_example.c
@@ -3,13 +3,13 @@
 #include "../src/gaussian_pulse.h"
 
 int main() {
-    #define LEN 200
+    #define LEN 500
     float t[LEN], real[LEN];
-    for (int i = -100; i < LEN/2; i++) {
-        t[i+100] = (float)i/100;
+    for (int i = -LEN/2; i < LEN/2; i++) {
+        t[i+LEN/2] = (float)i/(LEN/2);
     }
 
-    gaussian_pulse(t, real, LEN, 5, 0.5, -6);
+    gaussian_pulse(t, real, LEN, 5, 0.4, -6);
     FILE *f = fopen("gauss_pulse.csv", "w");
 
     for (int i = 0; i < LEN; i++) {
